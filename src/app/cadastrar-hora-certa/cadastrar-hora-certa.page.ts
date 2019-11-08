dd65f2426654c2868da108dcc6b6367c94643f36
Damon Dudek <damondudek@outlook.com>
2019-11-07 21:25:33 -0300

    adicionado a página de cadastro e os componentes de data

---
+++ src/app/cadastrar-hora-certa/cadastrar-hora-certa.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cadastrar-hora-certa',
+  templateUrl: './cadastrar-hora-certa.page.html',
+  styleUrls: ['./cadastrar-hora-certa.page.scss'],
+})
+export class CadastrarHoraCertaPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
