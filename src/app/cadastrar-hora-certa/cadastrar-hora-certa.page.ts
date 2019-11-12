786885b5698dfca71e37b1cd6442a8d3b3ee3fd5
Damon Dudek <damondudek@outlook.com>
2019-11-11 21:36:44 -0300

    Adicionado inputs e ionic storage

--- src/app/cadastrar-hora-certa/cadastrar-hora-certa.page.ts
+++ src/app/cadastrar-hora-certa/cadastrar-hora-certa.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+
 @Component({
   selector: 'app-cadastrar-hora-certa',
   templateUrl: './cadastrar-hora-certa.page.html',
@@ -7,9 +10,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CadastrarHoraCertaPage implements OnInit {
 
+  formCadastrarHoraCerta = new FormGroup({
+    data: new FormControl('', Validators.required),
+    hora: new FormControl('', Validators.required),
+    falarTexto: new FormControl('', Validators.required),
+    cor: new FormControl('', Validators.required)
+  });
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  cadastrarHoraCerta() {
+    console.log(this.formCadastrarHoraCerta.value);
   }
 
 }
